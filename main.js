22e6659d3d84af17c6e2b13f1b27711da0f4f41b
Arjun Mehta <arjunmeht@gmail.com>
2014-04-08 10:00:58 -0400

    Added initialization methods and optimizations

--- main.js
+++ main.js
@@ -24,8 +24,25 @@
  */
 
 var geohash = require('ngeohash');
-
 var redis_clientZSetName = "geohashzset";
+var redis_client;
+
+var initialize = function(client, zSetName){
+  redis_clientZSetName = zSetName || "geohashzset";
+  redis_client = client;
+};
+
+var setGeo = function(options){
+
+  if(options.client !== undefined){
+    redis_client = options.client;
+  }
+  if(options.zset !== undefined){
+    redis_clientZSetName = options.zset;
+  }
+
+};
+
 
 /**
  * Range-Radius Index
@@ -92,9 +109,11 @@ var rangeDepth = function(radius){
  * @param {Number} bitDepth (bit depth of final hash values)
  * @returns {Hash Integer Ranges} Array
  */
-var hashIntegerRangesforBitDepth = function(lat, lon, rBitDepth, bitDepth){
+var hashIntegerRangesforBitDepth = function(lat, lon, rBitDepth, bitDepth, callBack){
 
-  bitDepth = bitDepth || 52;  
+  // var startTime = new Date().getTime();
+
+  bitDepth = bitDepth || 52;
   rBitDepth = rBitDepth || 48;
 
   var bitDiff = bitDepth - rBitDepth;
@@ -102,23 +121,38 @@ var hashIntegerRangesforBitDepth = function(lat, lon, rBitDepth, bitDepth){
     throw "bitDepth must be high enough to calculate range within radius";
   }
 
-  var hash = geohash.encode_int(lat, lon, rBitDepth);
-  var ranges = [];
-  var neighbors = geohash.neighbors_int(hash, rBitDepth);
+  var i, j;
 
-  for(var i=0; i< neighbors.length; i++){
-    ranges.push(buildRange(neighbors[i], bitDiff));
+  var ranges = [];
+  var range;
+
+  var lowerRange = 0,
+      upperRange = 0;
+
+  var hash = geohash.encode_int(lat, lon, rBitDepth);
+  var neighbors = geohash.neighbors_int(hash, rBitDepth);
+  
+  neighbors.push(hash);
+  neighbors.sort();
+
+  for(i=0; i<neighbors.length; i++){
+    lowerRange = neighbors[i];
+    upperRange = lowerRange + 1;    
+    while(neighbors[i+1] === upperRange){
+      neighbors.shift();
+      upperRange = neighbors[i]+1;
+    }    
+    ranges.push([lowerRange, upperRange]);
+  }
+  
+  for(i=0; i<ranges.length; i++){
+    range = ranges[i];
+    range[0] = leftShift(range[0], bitDiff);
+    range[1] = leftShift(range[1], bitDiff);
   }
 
   return ranges;
 };
-
-function buildRange(hash_integer, diff){
-
-  var lowerRange = leftShift(hash_integer, diff);
-  var upperRange = leftShift(hash_integer + 1, diff);
-  return [lowerRange, upperRange];
-}
 
 function leftShift(integer, shft){
   return integer * Math.pow(2, shft);
@@ -136,20 +170,12 @@ function leftShift(integer, shft){
  * @param {Number} bitDepth (bit depth of final hash values)
  * @returns {Hash Integer Ranges} Array
  */
-var hashIntegerRangesforRadius = function(lat, lon, radius, bitDepth){
+var hashIntegerRangesforRadius = function(lat, lon, radius, bitDepth, callBack){
   var rBitDepth = rangeDepth(radius);
-  return hashIntegerRangesforBitDepth(lat, lon, rBitDepth, bitDepth);
-};
+  console.log("RADIUS BIT DEPTH:", rBitDepth);
 
-/**
- * Set Client Name
- *
- * Sets the default zset name for various redis operations.
- *
- * @param {String} zset_name
- */
-var redis_setClientZSetName = function(zset_name){
-  redis_clientZSetName = zset_name;
+  // hashIntegerRangesforBitDepth(lat, lon, rBitDepth, bitDepth, callBack);
+  return hashIntegerRangesforBitDepth(lat, lon, rBitDepth, bitDepth);
 };
 
 /**
@@ -159,21 +185,53 @@ var redis_setClientZSetName = function(zset_name){
  * @param {Hash Integer Ranges} ranges
  * @param {Function} callBack
  */
-var redis_nearbySearch = function(client, ranges, callBack){
+var redis_hashRangeSearch = function(client, ranges, callBack){
 
-  var rangeCount = 0;
   var rangeLength = ranges.length;
-  var args = [];
+  var range = [];
+  var i;
 
   var multi = client.multi();
-  for(var i=0; i<rangeLength; i++){
-    multi.ZRANGEBYSCORE("myzset", ranges[i][0], ranges[i][1]);
+  for(i=0; i<rangeLength; i++){
+    range = ranges[i];
+    multi.ZRANGEBYSCORE("myzset", range[0], range[1]);
   }
 
-  multi.exec(function (err, replies) {
-    if(callBack && typeof callBack == "function") callBack(err, replies);
+  multi.exec(function(err, replies){
+    var concatedReplies = [];
+    for(i=0; i< replies.length; i++){
+      concatedReplies = concatedReplies.concat(replies[i]);
+    }
+    if(callBack && typeof callBack === "function") callBack(err, concatedReplies);
   });
+
 };
+
+
+var redis_findNearby = function(lat, lon, options, callBack){
+
+  if(typeof options === "function" && callBack === undefined){
+    callBack = options;
+    options = {};
+  }
+
+  var ranges;
+  var client = options.client || redis_client;
+
+  if(options.ranges === undefined){
+    var bitDepth = options.bitDepth || 52;
+    var rBitDepth = (options.radius !== undefined) ? options.radius : (options.rBitDepth || 48);  
+    ranges = hashIntegerRangesforBitDepth(lat, lon, rBitDepth, bitDepth);
+  }
+  else{
+    ranges = options.ranges;
+  }  
+
+  // console.log(options.ranges);
+  
+  redis_hashRangeSearch(client, ranges, callBack);
+};
+
 
 /**
  * Add New Redis Coordinate (Asynchronous)
@@ -185,7 +243,7 @@ var redis_nearbySearch = function(client, ranges, callBack){
  * @param {Number} bit_Depth (defaults to 52)
  * @param {Function} callBack
  */
-function redis_addNewCoordinate(client, lat, lon, key_name, bitDepth, callBack){
+var redis_addNewCoordinate = function(client, lat, lon, key_name, bitDepth, callBack){
 
   if(typeof bitDepth === "function"){
     callBack = bitDepth;
@@ -195,21 +253,104 @@ function redis_addNewCoordinate(client, lat, lon, key_name, bitDepth, callBack){
     bitDepth = bitDepth || 52;
   }
 
-  client.zadd(redis_clientZSetName, geohash.encode_int(lat, lon, bitDepth), key_name, function (err, res){
-    if(err){
-      if(callBack && typeof callBack === "function") callBack(err, res);
+  client.zadd(redis_clientZSetName, geohash.encode_int(lat, lon, bitDepth), key_name, callBack);
+};
+
+
+var redis_findCoordinatesInRangeNaive = function(client, lat, lon, radius, callBack){
+
+  var ranges = getMinMaxs(lat, lon, radius);
+
+  var multi = client.multi();
+      multi.ZUNIONSTORE("temp:lat", 1, "index:lat", "WEIGHTS", 1);
+      multi.ZREMRANGEBYSCORE("temp:lat", 0, ranges.latmin);
+      multi.ZREMRANGEBYSCORE("temp:lat", ranges.latmax, "INF");
+      multi.ZUNIONSTORE("temp:lon", 1, "index:lon", "WEIGHTS", 1);
+      multi.ZREMRANGEBYSCORE("temp:lon", 0, ranges.lonmin);
+      multi.ZREMRANGEBYSCORE("temp:lon", ranges.lonmax, "INF");
+      multi.ZINTERSTORE("temp:result", 2, "temp:lon", "temp:lat");
+      multi.ZRANGE("temp:result", 0, -1, callBack);
+
+      multi.exec(callBack);
+};
+
+
+var redis_findCoordinatesInRangeMin = function(client, lat, lon, radius, callBack){
+
+  var startTime = new Date().getTime();
+
+  var ranges = getMinMaxs(lat, lon, radius);
+
+  var multi = client.multi();
+      multi.ZRANGEBYSCORE("index:lat", ranges.latmin, ranges.latmax);
+      multi.ZRANGEBYSCORE("index:lon", ranges.lonmin, ranges.lonmax);
+      multi.exec(function(err, replies){
+
+        // console.log("TIMESTAMP REDIS RESULT for Coordinate Range", new Date().getTime()-startTime);
+        var replyLength = 0;
+        for(var i=0; i<replies.length; i++){
+          replyLength += replies[i].length;
+        }
+        // console.log("FOR REPLY LENGTH:",replyLength);
+
+        startTime = new Date().getTime();
+
+        var lats = arrayToObject(replies[0]);
+        var lons = arrayToObject(replies[1]);
+
+        var intersection = (replies[0].length < replies[1].length) ? intersect(lats, lons) : intersect(lons, lats);
+
+        if(callBack && typeof callBack == "function") callBack(err, intersection);
+        // console.log(replies);
+      });
+};
+
+
+
+// HELPER FUNCTIONS
+
+function getMinMaxs(lat, lon, radius){
+
+  var latr = radius/111111;
+  var lonr = radius/(111111 *  Math.abs(Math.cos(lat)));
+
+  var latmin = lat - latr;
+  var latmax = lat + latr;
+  var lonmin = lon - lonr;
+  var lonmax = lon + lonr;
+
+  return {latmin: latmin, latmax: latmax, lonmin: lonmin, lonmax: lonmax};
+}
+
+function arrayToObject(a){
+  var o = {};
+  for(var i=0; i<a.length; i++){
+    o[a[i]] = {};
+  }
+  return o;
+}
+
+function intersect(a, b){
+  var intersection = {};
+  var key;
+  for(key in a){
+    if(b[key] !== undefined){
+      intersection[key] = {};
     }
-  });
+  }
+  return intersection;
 }
 
 
+
 var geohashDistance = {
-  'rangeDepth': rangeDepth,
   'hashIntegerRangesforBitDepth':hashIntegerRangesforBitDepth,
   'hashIntegerRangesforRadius':hashIntegerRangesforRadius,
-  'redis_setClientZSetName': redis_setClientZSetName,
-  'redis_nearbySearch': redis_nearbySearch,
-  'redis_addNewCoordinate': redis_addNewCoordinate
+  'redis_hashRangeSearch': redis_hashRangeSearch,
+  'redis_findNearby': redis_findNearby,
+  'redis_addNewCoordinate': redis_addNewCoordinate,
+  'redis_findCoordinatesInRangeNaive': redis_findCoordinatesInRangeNaive,
+  'redis_findCoordinatesInRangeMin': redis_findCoordinatesInRangeMin
 };
 
 
